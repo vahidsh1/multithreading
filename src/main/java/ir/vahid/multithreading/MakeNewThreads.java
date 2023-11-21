fdfc0d6ae7391bfafe83575e5d916bc34653d0e3
vahidsh1 <vahidsh1@gmail.com>
2023-11-21 12:01:22 +0330

    modify semaphore

--- src/main/java/ir/vahid/multithreading/MakeNewThreads.java
+++ src/main/java/ir/vahid/multithreading/MakeNewThreads.java
@@ -6,7 +6,10 @@ class Shared {
     public static int number=0;
 }
 public class MakeNewThreads extends Thread {
-    Semaphore semaphore = new Semaphore(1);
+    Semaphore semaphore ;
+public MakeNewThreads (Semaphore semaphore) {
+    this.semaphore = semaphore;
+}
     public void run(){
 //        System.out.println(" Thread name : " + Thread.currentThread().getName());
 //        System.out.println(" Thread State : " + Thread.currentThread().getState());
@@ -15,9 +18,9 @@ public class MakeNewThreads extends Thread {
             semaphore.acquire();
             System.out.println(Thread.currentThread().getName()  + " gets a permit.");
             for(int count=0;count<10;count++) {
-                System.out.println("count number is : " + Shared.number + " " + Thread.currentThread().getName() + " available permit is: " + semaphore.availablePermits());
+                System.out.println("count number is : " + Shared.number + " " + Thread.currentThread().getName() + " available permit is: " + semaphore.getQueueLength());
                 Shared.number++;
-                Thread.sleep(10);
+//                Thread.sleep(10);
             }
 //            semaphore.release();
         } catch (InterruptedException e) {
@@ -25,18 +28,19 @@ public class MakeNewThreads extends Thread {
         }
         System.out.println(Thread.currentThread().getName() + " releases the permit.");
         semaphore.release();
+        System.out.println(semaphore.availablePermits() + " numbers of available permits.");
 
     }
 
     public static void main(String[] args) {
-        MakeNewThreads t1 = new MakeNewThreads();
-        MakeNewThreads t2 = new MakeNewThreads();
-        MakeNewThreads t3 = new MakeNewThreads();
-        MakeNewThreads t4 = new MakeNewThreads();
+        Semaphore semaphore = new Semaphore(1);
+        MakeNewThreads t1 = new MakeNewThreads(semaphore);
+        MakeNewThreads t2 = new MakeNewThreads(semaphore);
+        MakeNewThreads t3 = new MakeNewThreads(semaphore);
+        MakeNewThreads t4 = new MakeNewThreads(semaphore);
         t1.start();
         t2.start();
         t3.start();
         t4.start();
-//        Semaphore semaphore = new Semaphore(2);
     }
 }
