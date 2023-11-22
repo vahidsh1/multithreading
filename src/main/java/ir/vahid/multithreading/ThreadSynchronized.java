7007d154927839f9b77b9bf4c8c1cc52bfb38546
vahidsh1 <vahidsh1@gmail.com>
2023-11-22 15:18:29 +0330

    Thread Priority Example

--- src/main/java/ir/vahid/multithreading/ThreadSynchronized.java
+++ src/main/java/ir/vahid/multithreading/ThreadSynchronized.java
@@ -47,3 +47,25 @@ class SyncDemo {
         }
     }
 }
+//Explanation
+//        In the above example, we choose to synchronize the Sender object inside the run() method of the ThreadedSend class.
+//        Alternately, we could define the whole send() block as synchronized, producing the same result.
+//        Then we don’t have to synchronize the Message object inside the run() method in ThreadedSend class.
+
+
+//    Process Synchronization
+//    Thread Synchronization
+//        1. Process Synchronization in Java
+//        Process Synchronization is a technique used to coordinate the execution of multiple processes. It ensures that the shared resources are safe and in order.
+//
+//        2. Thread Synchronization in Java
+//        Thread Synchronization is used to coordinate and ordering of the execution of the threads in a multi-threaded program. There are two types of thread synchronization are mentioned below:
+//
+//        Mutual Exclusive
+//        Cooperation (Inter-thread communication in Java)
+//        Mutual Exclusive
+//        Mutual Exclusive helps keep threads from interfering with one another while sharing data. There are three types of Mutual Exclusive mentioned below:
+//
+//        Synchronized method.
+//        Synchronized block.
+//        Static synchronization.
